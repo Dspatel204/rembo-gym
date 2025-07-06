df9458eeaf1419e16a143966e1d8a859131e94e3
Dspatel204 <dishant.sureshbhai@gmail.com>
2025-07-06 12:19:06 +0530

    add upadte

--- src/components/contact-page/Info.jsx
+++ src/components/contact-page/Info.jsx
@@ -15,32 +15,28 @@ function Info() {
         <div>
           <h4 className={headingStyles}>Karachi, Pakistan</h4>
           <address className="font-medium not-italic text-gray-300">
-            Shah Faisal Colony
-            <br /> Karachi
+            Radhika complex
+            <br /> surat
           </address>
         </div>
         <div>
           <h4 className={headingStyles}>Opening Hours</h4>
           <p className="whitespace-nowrap font-medium text-gray-300">
-            Mon to Fri: 9:00 am — 10:00 pm
-          </p>
-          <p className="whitespace-nowrap font-medium text-gray-300">
-            Sat: 10:30 pm — 12:00 am
+            Mon to Sat: 6:00 am — 10:00 pm
           </p>
         </div>
         <div>
           <h4 className={headingStyles}>Information</h4>
           <a
-            href="tel:+98-21-225-429-71"
+            href="tel:+91 9586471510"
             className="block font-medium text-gray-300"
           >
-            +92 31523697586
+            +91 9586471510
           </a>
           <a
-            href="sharjeelsiddiqui00@gmail.com"
+            href="dishant.sureshbhai@gmail.com"
             className="block font-medium text-gray-300"
-          >
-            sharjeelsiddiqui00@gmail.com
+          >dishant.sureshbhai@gmail.com
           </a>
         </div>
         <div>
